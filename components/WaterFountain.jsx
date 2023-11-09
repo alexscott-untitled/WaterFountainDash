86bea03ae05693ad59bb77e71c9f062b397e87e4
alexscott-untitled <alex.scott@untitledfirm.com>
2023-11-09 11:29:40 -0500

    Initial Water Fountain Stats

---
+++ components/WaterFountain.jsx
@@ -0,0 +1,4 @@
+//insert bottle count with event listener
+const bottleCount = 6250
+const waterOunces = bottleCount * 20
+const gallons = waterOunces * 128
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <h1>Precursor</h1>
+                <h1>Water Fountain Stats</h1>
             </main>
         </div>
     )
